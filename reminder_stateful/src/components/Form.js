bd983b74240f956a791ed4ffe1b62cd0b496146f
zlatka-n <zlatka@Bichs-MacBook-Pro.local>
2021-06-26 21:52:45 +0200

    edit button added

--- reminder_stateful/src/components/Form.js
+++ reminder_stateful/src/components/Form.js
@@ -1,6 +1,22 @@
 import React from "react";
 
-const Form = ({ onInputChange, onFormSubmit, value }) => {
+const Form = ({
+  onInputChange,
+  onFormSubmit,
+  value,
+  editValue,
+  onEditClick,
+  onEditChange,
+}) => {
+  if (editValue) {
+    return (
+      <form onSubmit={onEditClick}>
+        <input value={editValue.value} onChange={onEditChange}></input>
+        <button>Update</button>
+      </form>
+    );
+  }
+
   return (
     <form onSubmit={onFormSubmit}>
       <input onChange={onInputChange} value={value.text}></input>
